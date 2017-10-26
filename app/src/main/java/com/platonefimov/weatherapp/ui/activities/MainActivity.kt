f8154a5571d9107d72c664b7977916edd1365bbc
Platon Efimov <Platon-E-S@yandex.ru>
2017-10-27 02:39:49 +0700

    Mock-up deleting

--- app/src/main/java/com/platonefimov/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/platonefimov/weatherapp/ui/activities/MainActivity.kt
@@ -12,26 +12,12 @@ import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
-    private val items = listOf("Mon 6/23 - Sunny - 31/17",
-            "Tue 6/24 - Foggy - 21/8",
-            "Wed 6/25 - Cloudy - 22/17",
-            "Thurs 6/26 - Rainy - 18/11",
-            "Fri 6/27 - Foggy - 21/10",
-            "Sat 6/28 - TRAPPED IN WEATHER STATION - 23/18",
-            "Sun 6/29 - Sunny - 20/7"
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
-        forecastList.adapter = ForecastListAdapter(items)
-
-        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
-                "APPID=a163eb06ba400dc97554789d6d39fc18" +
-                "&q=94043&mode=json&units=metric&cnt=7"
 
         doAsync {
             val result = RequestForecastCommand("634045").execute()
