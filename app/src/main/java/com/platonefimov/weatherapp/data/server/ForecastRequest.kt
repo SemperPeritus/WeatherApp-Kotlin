272ec6990c4fd63913f0d82474a7e8cbbde58932
Platon Efimov <platon.efimov@gmail.com>
2017-11-13 18:24:18 +0700

    Database

--- app/src/main/java/com/platonefimov/weatherapp/data/server/ForecastRequest.kt
+++ app/src/main/java/com/platonefimov/weatherapp/data/server/ForecastRequest.kt
@@ -3,13 +3,13 @@ package com.platonefimov.weatherapp.data.server
 import com.google.gson.Gson
 import java.net.URL
 
-class ForecastRequest(val zipCode: Long) {
+class ForecastRequest(private val zipCode: Long) {
 
     companion object {
         private val APP_ID = "a163eb06ba400dc97554789d6d39fc18"
         private val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json" +
                 "&units=metric&cnt=7"
-        private val COMPLETE_URL = "${URL}&APPID=${APP_ID}&zip="
+        private val COMPLETE_URL = "$URL&APPID=$APP_ID&zip="
     }
 
     fun execute(): ForecastResult {
